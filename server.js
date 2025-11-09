2f699fad943ce24c7818e1b3f3dabd961b2b252f
Girish Thorat <151001374+GirishT25@users.noreply.github.com>
2025-11-09 15:45:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
-require('dotenv').config();
-const express = require('express')
-const app = express();
-app.use(express.json());
-const Database = require('./database/DB.js')
-const routes = require('./routes/auth-routes.js');
-const Homeroutes = require('./routes/home-route.js');
-const AdminRoutes = require('./routes/admin-route.js');
-const uploadRoutes = require('./routes/image-upload.js');
+const http =  require('http');
 
+const server = http.createServer((req , res)=>{ // the Http server is created and the req and res is created in the callback
+console.log(req, 'Hello'); // it get the request from the server 
+res.writeHead(200 , {"Content-type" :"text/plain"}); // this is writeHead i.e it written the content in plain format text
+res.end('Hello from the Nodejs'); // it send the response form the server end
 
-// database connection
-Database();
-// console.log("Database connected successfully 25 ")
+});
 
-const PORT = process.env.PORT || 3000
-
-app.use('/api/auth' , routes);
-app.use('/api/home' , Homeroutes);
-app.use('/api/admin' , AdminRoutes);
-app.use('/api/image' , uploadRoutes);
-
-
-
-app.listen(PORT , ()=>{
-    console.log(`Server is listening on the ${PORT}`);
-})
+const port = 3000;
+server.listen(port ,()=>{
+    console.log(`Server is listening on the port ${port}`);
+});
